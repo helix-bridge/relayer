7a805198dcb33c7ffbe7907ced3edd2686096f06
xiaoch05 <xiaoch2010@gmail.com>
2024-01-09 22:00:58 +0800

    upgrade ethers

--- src/base/safewallet.ts
+++ src/base/safewallet.ts
@@ -1,7 +1,12 @@
-import { SafeTransactionDataPartial, SafeMultisigTransactionResponse, SafeMultisigConfirmationResponse } from '@safe-global/safe-core-sdk-types';
+import {
+    SafeTransactionDataPartial,
+    SafeMultisigTransactionResponse,
+    SafeMultisigConfirmationResponse,
+    MetaTransactionData
+} from '@safe-global/safe-core-sdk-types';
 import Safe, {EthersAdapter} from '@safe-global/protocol-kit';
 import SafeApiKit from '@safe-global/api-kit'
-import { ethers } from 'ethers';
+import { ethers, Wallet, HDNodeWallet } from "ethers";
 const ApiKit = require('@safe-global/api-kit');
 
 type Opts = {
@@ -20,23 +25,23 @@ export interface TransactionPropose {
 export class SafeWallet {
     public address: string;
     public apiService: string;
-    public signer: ethers.Wallet;
+    public signer: Wallet | HDNodeWallet;
     private safeSdk: Safe;
     private safeService: SafeApiKit;
-    constructor(address: string, apiService: string, signer: ethers.Wallet){
+    constructor(address: string, apiService: string, signer: Wallet | HDNodeWallet){
         this.address = address;
         this.signer = signer;
         this.apiService = apiService;
     }
 
-    async connect() {
+    async connect(chainId: bigint) {
         const ethAdapter = new EthersAdapter({
             ethers,
             signerOrProvider: this.signer,
         });
 
         this.safeSdk = await Safe.create({ ethAdapter: ethAdapter, safeAddress: this.address })
-        this.safeService = new SafeApiKit({ txServiceUrl: this.apiService, ethAdapter })
+        this.safeService = new SafeApiKit({ txServiceUrl: this.apiService, chainId })
     }
 
     private isTransactionSignedByAddress(tx: SafeMultisigTransactionResponse): boolean {
@@ -71,14 +76,20 @@ export class SafeWallet {
         address: string,
         data: string,
         isProposor: boolean,
+        chainId: bigint,
         value: string = '0'): Promise<TransactionPropose | null> {
-        this.safeSdk ?? await this.connect();
-        const safeTransactionData: SafeTransactionDataPartial = {
+        this.safeSdk ?? await this.connect(chainId);
+
+        const transactions: MetaTransactionData[] = [
+          {
             to: address,
             value,
             data
-        };
-        const tx = await this.safeSdk.createTransaction({ safeTransactionData });
+          },
+        ]
+
+        const tx = await this.safeSdk.createTransaction({ transactions });
+
         const safeTxHash = await this.safeSdk.getTransactionHash(tx);
         try {
             const transaction = await this.safeService.getTransaction(safeTxHash);
